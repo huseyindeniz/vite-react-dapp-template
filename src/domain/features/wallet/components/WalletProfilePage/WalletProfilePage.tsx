d52e2b527d255b3a8162f6a91e5f129d6710d051
huseyindeniz <huseyindenizkivrak@gmail.com>
2025-11-16 20:45:45 +0200

    fix wallet profile page.

--- src/domain/features/wallet/components/WalletProfilePage/WalletProfilePage.tsx
+++ src/domain/features/wallet/components/WalletProfilePage/WalletProfilePage.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
 
-import { Box, Container } from '@mantine/core';
-import { useTranslation } from 'react-i18next';
-
 import { Header } from './Header';
 
 export const WalletProfilePage: React.FC = () => {
-  const { t } = useTranslation('feature-wallet');
-  return (
-    <Container>
-      <Header />
-      <Box mt={10} ta="center">
-        {t(
-          "As an example, you can put current user's latest transactions here..."
-        )}
-      </Box>
-    </Container>
-  );
+  return <Header />;
 };
